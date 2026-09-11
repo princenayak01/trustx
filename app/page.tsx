e874cc88e10db7ce839182d423fff3c2d7cf5ded
Prince Nayak <Pn0562058@gmail.com>
2026-09-11 11:39:51 +0530

    feat: protect TrustX dashboard with authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import TrustXV2 from '@/components/trustx-v2'
 import TrustXLiveCenter from '@/components/trustx-live-center'
+import AuthGate from '@/components/auth-gate'
 
 export default function Page() {
   return (
-    <>
+    <AuthGate>
       <TrustXV2 />
       <TrustXLiveCenter />
-    </>
+    </AuthGate>
   )
 }
